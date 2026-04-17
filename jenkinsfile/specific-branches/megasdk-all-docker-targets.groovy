d0cbb721cc06bd3050c412aefd7a6673aa854ebf
Roddy González <rbg@mega.co.nz>
2026-04-17 16:52:01 +0200

    Fix pipeline issues
    And address reviewers comments

--- jenkinsfile/specific-branches/megasdk-all-docker-targets.groovy
+++ jenkinsfile/specific-branches/megasdk-all-docker-targets.groovy
@@ -5,7 +5,7 @@ def getImageTag (String distro, String build_id) {
 }
 
 pipeline {
-    agent { label 'linux && docker' }
+    agent { label 'docker' }
     options {
         timeout(time: 300, unit: 'MINUTES')
         buildDiscarder(logRotator(numToKeepStr: '135', daysToKeepStr: '21'))
@@ -15,9 +15,27 @@ pipeline {
     parameters {
         booleanParam(name: 'RESULT_TO_SLACK', defaultValue: true, description: 'Should the job result be sent to slack?')
         string(name: 'SDK_BRANCH', defaultValue: 'develop', description: 'Define a custom SDK branch.')
+        choice(name: 'DISTRO',
+            choices: [
+                'ubuntu:24.04', // Default value
+                'debian:11',
+                'debian:12',
+                'debian:13',
+                'debian:testing',
+                'archlinux',
+                'almalinux:9',
+                'centos:stream9',
+                'opensuse/leap:16.0',
+                'opensuse/tumbleweed',
+                'ubuntu:24.04',
+                'ubuntu:25.10',
+                'fedora:43',
+                'fedora:44'
+            ],
+            description: 'Distribution to build')
     }
     stages {
-        stage('checkout'){
+        stage('checkout') {
             steps {
                 checkout([
                     $class: 'GitSCM',
@@ -29,45 +47,31 @@ pipeline {
                 ])
             }
         }
-        stage('build'){
-            matrix {
-                axes {
-                    axis {
-                        name 'DISTRO'
-                        values  'debian:11',
-                                'debian:12',
-                                'debian:testing',
-                                'opensuse/tumbleweed',
-                                'ubuntu:20.04',
-                                'ubuntu:22.04',
-                                'ubuntu:24.04',
-                                'ubuntu:24.10'
-                    }
+        stage("build image") {
+            environment {
+                IMAGE_TAG = getImageTag("$DISTRO", "$BUILD_ID")
+            }
+            steps {
+                script {
+                    sh """
+                        docker build \
+                          --build-arg DISTRO=${DISTRO} \
+                          --build-arg ARCH=amd64 \
+                          -f dockerfile/linux-build.dockerfile \
+                          -t ${IMAGE_TAG} \
+                          .
+                    """
+                    sh """
+                        docker run \
+                            -v ${WORKSPACE}:/mega/sdk:ro \
+                    ${IMAGE_TAG}
+                    """
                 }
-                stages{
-                    stage("build image") {
-                        environment {
-                            IMAGE_TAG = getImageTag("$DISTRO", "$BUILD_ID")
-                        }
-                        steps {
-                            script {
-                                sh """
-                                    docker build \
-                                      --build-arg DISTRO=${DISTRO} \
-                                      --build-arg ARCH=amd64 \
-                                      -f dockerfile/linux-build.dockerfile \
-                                      -t ${IMAGE_TAG} \
-                                      .
-                                """
-                            }
-                        }
-                        post {
-                            always {
-                                script {
-                                    sh "docker rm -f ${IMAGE_TAG}"
-                                }
-                            }
-                        }
+            }
+            post {
+                always {
+                    script {
+                        sh "docker rmi -f ${IMAGE_TAG}"
                     }
                 }
             }
@@ -81,7 +85,7 @@ pipeline {
                     def messageStatus = currentBuild.currentResult
                     def messageColor = messageStatus == 'SUCCESS'? "#00FF00": "#FF0000" //green or red
                     message = """
-                        *SDK Docker* <${BUILD_URL}|Build result>: '${messageStatus}'.
+                        *SDK Docker ${DISTRO}* <${BUILD_URL}|Build result>: '${messageStatus}'.
                         SDK branch: `${SDK_BRANCH}`
                         SDK commit: `${sdk_commit}`
                     """.stripIndent()
